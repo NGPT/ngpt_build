1e169c5ab24323f4deca788a6dfe3fbbb4e82fda
Sunsijie <sunsijie@buaa.edu.cn>
2016-04-04 11:11:33 +0800

    消除对mysql的依赖

--- docker/import.sh
+++ docker/import.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo docker exec -t build_pgsql_1 /bin/bash -c 'psql -U postgres -c "create database ngpt;"'
 sudo docker exec -it build_seed_1 /bin/bash -c '/var/www/html/yii migrate/up'
-mysql -h 127.0.0.1 -P 33060 -u root -p19941126  -f zhxs < forum.sql
+sudo docker cp ./forum.sql build_mysql_1:/home/
+sudo docker exec -it build_mysql_1 /bin/bash -c 'mysql -h 127.0.0.1 -u root -p19941126 -f zhxs < /home/forum.sql'
