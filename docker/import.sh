5b27d67e8c7af695f0fa19028bc114fcd20cf52a
Sunsijie <sunsijie@buaa.edu.cn>
2016-04-04 10:49:20 +0800

    开放数据库端口

--- docker/import.sh
+++ docker/import.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+sudo docker exec -t build_pgsql_1 /bin/bash -c 'psql -U postgres -c "create database ngpt;"'
+sudo docker exec -it build_seed_1 /bin/bash -c '/var/www/html/yii migrate/up'
 mysql -h 127.0.0.1 -P 33060 -u root -p19941126  -f zhxs < forum.sql
